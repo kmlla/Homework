9a45e939f4b3a51d2d82c4a6451a90fd38eee4f6
kmlla <93437435+kmlla@users.noreply.github.com>
2021-12-09 17:12:10 +0300

    Create Task7.cpp

---
+++ misc_cpp.md/Task7.cpp
@@ -0,0 +1,21 @@
+#include<cmath>
+#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "russian");
+	int R;
+	std::cin >> R;
+	int sum = 0;
+	for (int y = -(int)R; y <= (int)R; y+=1)
+	{
+		for (int x = -(int)R; x <= (int)R; x+=1)
+		{
+			if ((x*x + y*y) <= R*R)
+			{
+				++sum;
+			}
+		}
+	}
+
+	std::cout << sum;
+}
