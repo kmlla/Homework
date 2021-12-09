e7290e2e89a23d0ead595e37d5c8b97d2b6eb2c4
kmlla <93437435+kmlla@users.noreply.github.com>
2021-12-09 14:12:01 +0300

    Create Task4.cpp

---
+++ cpp_triangles.md/Task4.cpp
@@ -0,0 +1,34 @@
+int main()
+{
+    int n,q;
+    std::cin >> n >> q;
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int m = 1;m <= q;++m) 
+        {
+            for (int j = 1; j <= n - i; ++j)
+                std::cout << " ";
+            for (int k = 1;k <= 2 * i - 1;++k)
+                std::cout << "*";
+            for (int j = 1; j <= n - i; ++j)
+                std::cout << " ";
+        }
+        std::cout << '\n';
+
+    }
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int m = 1;m <= q;++m)
+        {
+            for (int j = 1; j <= n - i; ++j)
+                std::cout << " ";
+            for (int k = 1;k <= 2 * i - 1;++k)
+                std::cout << "*";
+            for (int j = 1; j <= n - i; ++j)
+                std::cout << " ";
+        }
+        std::cout << '\n';
+    }
+    return 0;
+}
