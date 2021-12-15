ba3b8e952d2f399aae71c72f8da41360f007f2ce
kmlla <93437435+kmlla@users.noreply.github.com>
2021-12-15 18:19:50 +0300

    Update Task3.cpp

--- misc_cpp.md/Task3.cpp
+++ misc_cpp.md/Task3.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 int main() {
-	setlocale(LC_ALL, "russian");
-	float x, y,R,r;
-	std::cout << "Введите х,у";
-	std::cin >> x >> y;
-	std::cout << "Введите R,r";
-	std::cin >> R >> r;
-	int a = x * x + y * y;
-	if ((a < R*R) && (a > r*r))
-	{
-		std::cout << "IN";
-	}
-	else {
-		std::cout << "OUT";
-	}
+    setlocale(LC_ALL, "russian");
+    double x, y, R, r;
+    std::cout << "Введите х,у";
+    std::cin >> x >> y;
+    std::cout << "Введите R,r";
+    std::cin >> R >> r;
+    double a = x * x + y * y;
+    if ((a < R * R) && (a > r * r))
+    {
+        std::cout << "IN";
+    }
+    else {
+        std::cout << "OUT";
+    }
 }
