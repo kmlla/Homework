3388f490b3c13f3c666a1231902de72dd5493c07
kmlla <93437435+kmlla@users.noreply.github.com>
2021-12-17 00:43:56 +0300

    Update Task2.cpp

--- misc_cpp.md/Task2.cpp
+++ misc_cpp.md/Task2.cpp
@@ -1,23 +1,27 @@
-#include <cmath> 
 #include <iostream>
-
+#include <cmath>
 int main()
 {
     double a, b, c, D, x1, x2; 
     std::cin >> a >> b >> c;
-    D = b * b - 4 * a * c; 
-    if (D > 0) 
-    {
-        x1 = ((-b) + sqrt(D)) / (2 * a);
-        x2 = ((-b) - sqrt(D)) / (2 * a);
-        std::cout << "two solutions: " << x1 << "," << x2 << "\n";
+    if (a == 0) {
+        std::cout << -(c / b);
     }
-    if (D == 0)
-    {
-        x1 = -(b / (2 * a));
-        std::cout << "one solution:" << x1 << "\n";
+    else {
+        D = b * b - 4 * a * c;
+        if (D > 0)
+        {
+            x1 = ((-b) + sqrt(D)) / (2 * a);
+            x2 = ((-b) - sqrt(D)) / (2 * a);
+            std::cout << "two solutions: " << x1 << "," << x2 << "\n";
+        }
+        if (D == 0)
+        {
+            x1 = -(b / (2 * a));
+            std::cout << "one solution:" << x1 << "\n";
+        }
+        if (D < 0)
+            std::cout << "no real solutions";
     }
-    if (D < 0) 
-        std::cout << "no real solutions";
     return 0;
 }
