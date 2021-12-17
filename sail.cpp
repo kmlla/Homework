5e6b3226e0c7bdd62a48e84ba705475dd1e26afe
kmlla <93437435+kmlla@users.noreply.github.com>
2021-12-17 14:00:37 +0300

    Update sail.cpp

--- sail.cpp
+++ sail.cpp
@@ -1,19 +1,16 @@
 #include<iostream>
 int main() {
-    float x, y;
+    double x;
+    double y;
     std::cin >> x >> y;
-    bool a = false;
-    if (((x + 1) * (x + 1) + y * y < 1) && (y <= 0)) 
-        a = true;
-    if (((x - 1) * (x - 1) + y * y < 1) && (y <= 0)) 
-        a = true;
-    if ((y <= x / 2 + 2) && (x <= 0) && (x >= -1) && (y >= 0)) 
-        a = true;
-    if ((y <= x / 2 + 2) && (x <= 0) && (x >= -1) && (y >= 0)) 
-        a = true;
-    if ((y <= -2 * x + 2) && (x <= 1) && (x >= 0) && (y >= 0)) 
-        a = true;
-
-    if (a) std::cout << "in";
-    if (!a) std::cout << "out";
+    if ((y >= 0) && (x <= 0) && (x >= -1) && (y <= 2 + 2 * x))
+        std::cout << "True" << '\n';
+    else if ((y >= 0) && (x >= 0) && (x <= 1) && (y <= -2 * x + 2))
+        std::cout << "True" << '\n';
+    else if ((y <= 0) && (x <= 0) && (((x + 1) * (x + 1) + y * y <= 1)))
+        std::cout << "True" << '\n';
+    else if ((y <= 0) && (x >= 0) && (((x - 1) * (x - 1) + y * y <= 1)))
+        std::cout << "True" << '\n';
+    else std::cout << "False" << '\n';
+    return 0;
 }
