db4e98361b904939311b70d25f8da88a40a0b627
kmlla <93437435+kmlla@users.noreply.github.com>
2021-12-26 11:54:57 +0300

    Update Task7.cpp

--- dyn_arrays/Task7.cpp
+++ dyn_arrays/Task7.cpp
@@ -2,25 +2,29 @@
 
 
 void shiftArray(int* a, int n, int k) {
-	for (int i = 1; i <= n; ++i) {
-		if (i <= k) {
-			a[n - k + i] = i;
-		}
-		else {
-			a[i - k] = i;
-		}
+	int* b = new int[n];
+	k %= n;
+	for (int i = 0; i < n; ++i) {
+		if (i < k)
+			b[n - k + i] = a[i];
+		else
+			b[i - k] = a[i];
 	}
+	for (int i = 0; i < n; ++i) 
+		a[i] = b[i];
+	delete[] b;
 }
+
 
 int main() {
 	int N, l;
 	std::cin >> N >> l;
 	int* arr = new int[N];
-	for (int i = 1; i <= N; ++i) {
+	for (int i = 0; i < N; ++i) {
 		std::cin >> arr[i];
 	}
 	shiftArray(arr, N, l);
-	for (int i = 1; i <= N; ++i) {
+	for (int i = 0; i < N; ++i) {
 		std::cout << arr[i] << " ";
 	}
 	delete[] arr;
