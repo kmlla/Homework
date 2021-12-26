e7b0c0744b1419a7d31139fbd97f8e2d1e65a8cc
kmlla <93437435+kmlla@users.noreply.github.com>
2021-12-26 12:09:35 +0300

    Update Task5.cpp

--- array/Task5.cpp
+++ array/Task5.cpp
@@ -4,10 +4,9 @@ int main() {
     int n;
     int a[1000];
     std::cin >> n;
-    for (int i = 1; i <= n; ++i) {
+    for (int i = 1; i <= n; ++i)
         std::cin >> i[a];
-    }
-    int sum = 0, t = 0;
+    int sum = a[0], t = 0;
     for (int i = 1; i <= n; ++i) {
         if ((a[i] + a[i + 1]) > sum) {
             sum = a[i] + a[i + 1];
