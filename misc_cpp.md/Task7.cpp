df935c9471b64253902a5b4ea7f88ff733b8e08b
kmlla <93437435+kmlla@users.noreply.github.com>
2021-12-27 14:04:18 +0300

    Update Task7.cpp

--- misc_cpp.md/Task7.cpp
+++ misc_cpp.md/Task7.cpp
@@ -1,15 +1,14 @@
+#include<cmath>
 #include <iostream>
 int main() {
     double R;
     std::cin >> R;
-    long long count = -1;
-    for (int y = 0; y <= R; ++y) {
-        for (int x = 0; x <= R; ++x) {
-            if ((x * x + y * y) <= R * R) {
-                count += 1;
-            }
-        }
-    }
-    std::cout << 1 + count * 4 - 4 * R;
-    return 0;
+    double sum = 0;
+    for (double y = 1.; y <= R; y += 1.)
+        for (double x = 1.; x <= R; x += 1.)
+            if ((x * x + y * y) <= R * R)
+                ++sum;
+    sum *= 4;
+    sum += 4 * R + 1;
+    std::cout << sum;
 }
