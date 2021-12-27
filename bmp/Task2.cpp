50ca7919d76d3701af24c52ba09a82fd10d251e4
kmlla <93437435+kmlla@users.noreply.github.com>
2021-12-27 13:11:14 +0300

    Update Task2.cpp

--- bmp/Task2.cpp
+++ bmp/Task2.cpp
@@ -3,22 +3,20 @@
 #include "ImgFunctions.h"
 
 
-typedef unsigned long ulong;
-
 RgbImg rotate_90(RgbImg const& image, bool clockwise = true)
 {
     RgbImg result = createRgbImg(image.width, image.height);
 
     if (clockwise == false)
     {
-        for (ulong row = 0; row < image.height; ++row)
-            for (ulong col = 0; col < image.width; ++col)
+        for (size_t row = 0; row < image.height; ++row)
+            for (size_t col = 0; col < image.width; ++col)
                 result.pixels[col][image.height - row - 1] = image.pixels[row][col];
     }
     else
     {
-        for (ulong row = 0; row < image.height; ++row)
-            for (ulong col = 0; col < image.width; ++col)
+        for (size_t row = 0; row < image.height; ++row)
+            for (size_t col = 0; col < image.width; ++col)
                 result.pixels[image.width - col - 1][row] = image.pixels[row][col];
     }
 
@@ -54,8 +52,8 @@ void rotate(char const* input, char const* output, unsigned int angle = 0)
     if (angle % 180 == 0)
     {
         RgbImg rotated = createRgbImg(image.height, image.width);
-        for (ulong row = 0; row < image.height; ++row)
-            for (ulong col = 0; col < image.width; ++col)
+        for (size_t row = 0; row < image.height; ++row)
+            for (size_t col = 0; col < image.width; ++col)
                 rotated.pixels[image.height - 1 - row][image.width - 1 - col] = image.pixels[row][col];
 
         writeRgbImg(output, rotated);
@@ -72,9 +70,9 @@ void rotate(char const* input, char const* output, unsigned int angle = 0)
 
 int main()
 {
-    rotate("rainbow.bmp", "90_rainbow.bmp", 90);
-    rotate("rainbow.bmp", "180_rainbow.bmp", 180);
-    rotate("rainbow.bmp", "270_rainbow.bmp", 270);
-    rotate("rainbow.bmp", "360_rainbow.bmp", 360);
+    rotate("rainbow.bmp", "90_rainbow1.bmp", 90);
+    rotate("rainbow.bmp", "180_rainbow1.bmp", 180);
+    rotate("rainbow.bmp", "270_rainbow1.bmp", 270);
+    rotate("rainbow.bmp", "360_rainbow1.bmp", 360);
     return 0;
 }
